b9d51ab37709e9978742be24aa155ee802c004aa
rhazama <rhazama01@gmail.com>
2022-05-09 07:14:01 -0700

    added functions in notes.js

--- Develop/lib/notes.js
+++ Develop/lib/notes.js
@@ -1,12 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
-function createNewNote() {
+function createNewNote(body, dbArray) {
+    const note = body.note;
+    dbArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(dbArray)
+    );
+    return note;
+}
 
-};
+function deleteNote(param, dbArray) {
+    const index = dbArray.findIndex(db => db.id === param);
 
-function deleteNote() {
-
+    db.Array.splice(index, 1);
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify(dbArray)
+    );
 }
 
 module.exports = { createNewNote, deleteNote };
